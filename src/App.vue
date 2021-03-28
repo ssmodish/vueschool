a12c12ee163bae5c067f654fd97d581877ad3fb3
Steve Smodish <smodish@hotmail.com>
2021-03-27 22:34:14 -0400

    vueschool

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div class="container">
+      <PageHome />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageHome from '@/components/PageHome'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PageHome
   }
 }
 </script>
---
+++ src/components/PageHome.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div v-for="thread in threads" 
+      :key="thread.id"
+      class="col-large push-top"
+    >
+      <h1>{{thread.title}}</h1>
+      <div class="post-list">
+        <div v-for="postId in thread.posts" :key="postId">
+          <p>{{userById(postById(postId).userId).name}}</p>
+          <p>{{postById(postId).text}}</p>
+        </div>
+        <div class="post" 
+          v-for="postId in thread.posts" 
+          :key="postId"
+        >
+          <div class="user-info">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+
+export default {
+  data() {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  },
+  methods: {
+    postById (postId) {
+      return this.posts.find(p => p.id === postId)
+    },
+    userById (userId) { 
+      return this.users.find(p => p.id === userId) 
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
